a0ae7be69185039a8b078fa4e56da884b5497bbb
Giuseppe Cassata <g.cassata@rheagroup.com>
2022-07-27 11:43:29 +0200

    Add existing project files to Git

---
+++ backend/src/main/java/com/izylife/izykube/IzykubeApplication.java
@@ -0,0 +1,15 @@
+package com.izylife.izykube;
+
+import lombok.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Generated
+@SpringBootApplication
+public class IzykubeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IzykubeApplication.class, args);
+  }
+
+}
