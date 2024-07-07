a1d6f4392391632c8385e30c398b579a58bfddcc
Giuseppe Cassata <giuseppe.cassata74@gmail.com>
2024-07-07 23:32:54 +0200

    added missing objects

--- backend/src/main/java/com/izylife/izykube/factory/NodeFactory.java
+++ backend/src/main/java/com/izylife/izykube/factory/NodeFactory.java
@@ -1,24 +1,42 @@
 package com.izylife.izykube.factory;
 
-import com.izylife.izykube.dto.cluster.ConfigMapDTO;
-import com.izylife.izykube.dto.cluster.NodeDTO;
-import com.izylife.izykube.dto.cluster.PodDTO;
-
+import com.izylife.izykube.dto.cluster.*;
 
 public class NodeFactory {
 
     public static NodeDTO createNodeDTO(NodeDTO node) {
-        switch (node.getKind()) {
+        switch (node.getKind().toLowerCase()) {
             case "configmap":
                 ConfigMapDTO configMap = (ConfigMapDTO) node;
                 return new ConfigMapDTO(configMap.getId(), configMap.getName(), configMap.getEntries());
             case "pod":
                 PodDTO pod = (PodDTO) node;
                 return new PodDTO(pod.getId(), pod.getName(), pod.getAssetId());
+            case "deployment":
+                DeploymentDTO deployment = (DeploymentDTO) node;
+                return new DeploymentDTO(
+                        deployment.getId(),
+                        deployment.getName(),
+                        deployment.getAssetId(),
+                        deployment.getReplicas(),
+                        deployment.getContainerPort(),
+                        deployment.getResources(),
+                        deployment.getEnvVars()
+                );
+            case "service":
+                ServiceDTO service = (ServiceDTO) node;
+                return new ServiceDTO(
+                        service.getId(),
+                        service.getName(),
+                        service.getType(),
+                        service.getPort(),
+                        service.getTargetPort(),
+                        service.getProtocol(),
+                        service.getNodePort()
+                );
             // Add cases for other node types as needed
             default:
-                return null;
+                throw new IllegalArgumentException("Unsupported node type: " + node.getKind());
         }
     }
-
 }
